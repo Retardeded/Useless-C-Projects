877bc17f8230d35285fe9fdf43614933c071a575
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2018-11-08 22:34:03 +0100

    Add files via upload

--- Basic.c
+++ Basic.c
@@ -6,15 +6,33 @@
 void main()
 {
 	int toSeconds = 60*60;
-	int toSecondsPerDay = toSeconds * 24;
-	int toSecondsPerWeek = toSecondsPerDay * 7;
+	float toDays = 0.04166666666;
+	float toWeeks = 0.00595238095;
 	int hours;
 	int n;
 	scanf("%d", &n);
 	hours = n;
-	printf("%d", hours);
-	printf("\n");
 	int seconds = hours * toSeconds;
-	printf("%i hours = %i", hours, seconds);
-	//printf("%i seconds = %i weeks %i days %i hours %i minutes %i seconds", seconds, seconds / toSecondsPerkWeek, hours * toSeconds)
+	float hoursFloat = (float)hours;
+	printf("%i hours = %i seconds \n", hours, seconds);
+	/*
+	if(hours == 0)
+	{
+		printf("00:00:00");
+	}
+	else if(hours <= 24)
+	{
+		printf("%i hours", hours);
+	}
+	else if(hours <= 24*7)
+	{
+		printf("%g days", hoursFloat*toDays);
+	}
+	else
+	{
+		printf("%g weeks", hoursFloat*toWeeks);
+	}
+	*/
+	printf("%i seconds = %g weeks %g days %i hours %i minutes %i seconds", seconds, hoursFloat*toWeeks,hoursFloat*toDays, hours, hours * 60, hours * toSeconds);
+	
 }
